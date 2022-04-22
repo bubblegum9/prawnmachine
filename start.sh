4cfc01c86c61e6d7e3f917dbaef85aee26bf606c
crackMaker <aleksa.andric064@gmail.com>
2022-04-22 22:03:42 +0200

    main.py prints to stdout and to log now

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-python3.9 main.py >> log
+# The tee command reads from the standard input and writes to both standard output and one or more files at the same time.
+python3.9 main.py | tee log
