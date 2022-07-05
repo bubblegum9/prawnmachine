8c33d039abd005a0c06bd333d9cef17c11a6ff37
crackMaker <aleksa.andric064@gmail.com>
2022-07-05 20:49:32 +0200

    Tee now appends

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # The tee command reads from the standard input and writes to both standard output and one or more files at the same time.
-python3 main.py | tee log
+python3 main.py | tee -a log
