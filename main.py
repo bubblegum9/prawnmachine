69991a7bf1fbe5e52bf44e6dbe643843c3131c16
crackMaker <aleksa.andric064@gmail.com>
2022-07-27 00:08:21 +0200

    NSFW checks SUCCESSFULLY IMPLEMENTED need to abstract, works only for cats not :/

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ in_user_agent    = "r/cat by u/LexCutter"
 delay = 0
 
 subreddit_list = {
-    'cats':         ['cats', catswebhook, delay],
-    'softwaregore': ['softwaregore', proxywebhook, delay],
-    'hardwaregore': ['hardwaregore', proxywebhook, delay],
-    'unixporn':     ['unixporn', proxyunixwebhook, delay]
+    'cats':         ['cats', catswebhook, delay]
+#    'softwaregore': ['softwaregore', proxywebhook, delay],
+#    'hardwaregore': ['hardwaregore', proxywebhook, delay],
+#    'unixporn':     ['unixporn', proxyunixwebhook, delay]
 }
 
 reddit = praw.Reddit(
@@ -27,41 +27,60 @@ reddit = praw.Reddit(
     user_agent=in_user_agent
 )
 
-print(">Connected!", "\n", ">Running Service...")
+print(">Connected!", "\n>Running Service...")
 
 
 def reddit_thread(subreddit_name, webhook, delay):
     try:
         for submission in reddit.subreddit(subreddit_name).stream.submissions(skip_existing=True):
-            print("FLAIR TEXT", submission.link_flair_text)
-            print(subreddit_name," >Got a Submission")
-            if 'i.redd.it' in submission.url:
-                print(subreddit_name," >>its a picture")
-                payload = {
-                    "content": None,
-                    "embeds": [
-                        {
-                            "title": str(submission.title),
-                            "description": None,
-                            "color":356357,
-                            "image":{
-                                "url":str(submission.url)
+            #print("FLAIR TEXT", submission.link_flair_text)
+            #print("IS OVER 18? ", submission.over_18, type(submission.over_18))
+
+            print(subreddit_name, " >Got a Submission")
+            print(subreddit_name, " > Checking if its not NSFW")
+            if not submission.over_18 and not any(x in submission.title for x in ["#porn", "porn", "nsfw", "NSFW"]):
+                print(subreddit_name, " > NOT NSFW")
+                print(subreddit_name, " > Checking if its a picture")
+                if 'i.redd.it' in submission.url and "Cat Picture" in submission.link_flair_text:
+                    print(subreddit_name," >>its a picture")
+                    payload = {
+                        "content": None,
+                        "embeds": [
+                            {
+                                "title": str(submission.title),
+                                "description": None,
+                                "color":356357,
+                                "image":{
+                                    "url":str(submission.url)
+                                }
                             }
+                        ]
+                    }
+                    response = r.post(webhook, json = payload)
+                    print(subreddit_name," >>>posted")
+                    print(response)
+                else:
+                    print(subreddit_name, " > NOT a picture")
+                    print(subreddit_name, " Was 'i.redd.it' in submission.url? ", 'i.redd.it' in submission.url)
+                    print(subreddit_name, " Was 'Cat Picture' in submission.link_flair_text? ", "Cat Picture" in submission.link_flair_text)
+
+                    print(subreddit_name, " > Checking if its a video")
+                    if submission.is_video and "Video" in submission.link_flair_text:
+                        print(subreddit_name," >>its a video")
+                        url_object = r.head(submission.url, allow_redirects=True)
+                        payload = {
+                            "content": str(url_object.url)
                         }
-                    ]
-                }
-                response = r.post(webhook, json = payload)
-                print(subreddit_name," >>>posted")
-                print(response)
-            if submission.is_video == True:
-                print(subreddit_name," >>its a video")
-                url_object = r.head(submission.url, allow_redirects=True)
-                payload = {
-                    "content": str(url_object.url)
-                }
-                response = r.post(webhook, json = payload)
-                print(subreddit_name," >>>posted")
-                print(response)
+                        response = r.post(webhook, json = payload)
+                        print(subreddit_name," >>>posted")
+                        print(response)
+                    else:
+                        print(subreddit_name, " > NOT a video")
+                        print(subreddit_name, " Did submission.is_video evaluate to True? ", submission.is_video)
+                        print(subreddit_name, " Was 'Video' in submission.link_flair_text? ", "Video" in submission.link_flair_text)
+
+                print(subreddit_name, " > DONE\n\n\n")
+                
             sleep(delay)
     except Exception as e:
         print('Error in ', subreddit_name, ': ', e)
